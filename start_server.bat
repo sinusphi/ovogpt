b37c9ec4e769c8cb441ececdb4e58bffe8207d94
sinusphi <sinusphi.sq@gmail.com>
2025-06-27 07:25:28 +0200

    add check

--- start_server.bat
+++ start_server.bat
@@ -1,5 +1,6 @@
 @echo off
-REM check if server is running and if so kill
+
+REM check if server is running and if yes kill it
 for /f "tokens=2 delims=," %%a in ('tasklist /FI "IMAGENAME eq python.exe" /FO CSV /NH') do (
     for /f "delims=" %%b in ('wmic process where "ProcessId=%%a and CommandLine like '%%ovo.py%%'" get ProcessId /value 2^>nul') do (
         set "line=%%b"
@@ -9,8 +10,18 @@ for /f "tokens=2 delims=," %%a in ('tasklist /FI "IMAGENAME eq python.exe" /FO C
     )
 )
 
-REM activate venv and start server
+REM check if venv exists
+if not exist ".venvs\ovo-venv\Scripts\activate.bat" (
+    echo [ERROR] No virtual environment found!
+    echo Please run setup.bat first.
+    pause
+    exit /b
+)
+
+REM activate venv
 call .venvs\ovo-venv\Scripts\activate.bat
+
+REM start server
 python ovogpt\ovo.py
 
 REM deactivate venv
